4c89ceedbc402caf4c9a85c1dbbf6a2bf2248bf6
dv-lebedev <dv.lebedev777@gmail.com>
2015-02-11 11:23:47 +0300

    added files

---
+++ Source/PairTradingView/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using PairTradingView.Forms;
+
+namespace PairTradingView
+{
+    static class Program
+    {
+        /// <summary>
+        /// Главная точка входа для приложения.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new MainWindow());
+        }
+    }
+}
