9f432cd71d96ae2f837ea9b4c6c43eab50daccbe
dv-lebedev <dv.lebedev777@gmail.com>
2015-06-06 16:41:12 +0300

    Updated

---
+++ Source/PairTradingView/Econometrics/Models/MultipleLinearRegression.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using PairTradingView.Econometrics.Basics;
+
+namespace PairTradingView.Econometrics.Models
+{
+    public class MultipleLinearRegression
+    {
+
+        private OrdinaryLeastSquares ols = null;
+
+        public double[] Coefs
+        {
+            get { return ols.Coefs; }
+        }
+
+        public double[] RValues
+        {
+            get { return ols.RValues; }
+        }
+
+        public double[] RSquaredValues
+        {
+            get { return ols.RSquaredValues; }
+        }
+
+
+        public MultipleLinearRegression()
+        {
+            ols = new OrdinaryLeastSquares();
+        }
+
+
+        public MultipleLinearRegression(double[] y, params double[][] xn)
+            : this()
+        {
+            this.Compute(y, xn);
+        }
+
+
+        public void Compute(double[] y, params double[][] xn)
+        {
+            ols.Compute(y, xn);
+        }
+
+    }
+}
